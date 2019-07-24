aa0cd7d5e7299ff04d6edccb2831a3109e4daa57
Tuomas Järvensivu <tuomas.jarvensivu@iki.fi>
2019-07-24 20:09:52 +0300

    Select correct processor based on operation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import { compile, execute } from "./processors/JsProcessor";
+
+enum Operation {
+  Compile = "compile",
+  Execute = "execute"
+}
+
+type ProcessorMap = {
+  [index in Operation]: any;
+};
+
+const processors: ProcessorMap = {
+  compile,
+  execute
+};
+
 class ArgumentList {
   private arguments: string[];
 
@@ -5,8 +21,25 @@ class ArgumentList {
     this.arguments = cliArgs;
   }
 
-  isValid() {
-    return this.arguments && this.arguments.length === 4;
+  isValidOperation(operation: string): boolean {
+    return Object.values(Operation).some(o => o === operation);
+  }
+
+  isValid(): boolean {
+    return (
+      this.arguments &&
+      this.arguments.length === 4 &&
+      this.isValidOperation(this.operationArgument())
+    );
+  }
+
+  getOperation(): Operation {
+    const operations = Object.values(Operation);
+    return operations.find(o => o === this.operationArgument());
+  }
+
+  private operationArgument(): string {
+    return this.arguments[2];
   }
 }
 
@@ -16,7 +49,8 @@ function printUsage() {
 
 const argumentList = new ArgumentList(process.argv);
 if (argumentList.isValid()) {
-  console.log("compiling/executing");
+  const processor = processors[argumentList.getOperation()];
+  processor();
 } else {
   printUsage();
   process.exit(1);
---
+++ src/processors/JsProcessor.ts
@@ -0,0 +1,7 @@
+export function compile() {
+  console.log("compiling javascript source code.");
+}
+
+export function execute() {
+  console.log("executing javascript source code.");
+}
