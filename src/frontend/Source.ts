14384522ed87ad15386c1d5d9be4635760a3792f
Tuomas Järvensivu <tuomas.jarvensivu@iki.fi>
2019-10-08 20:03:27 +0300

    Introduce source and tests for it

--- src/frontend/Source.ts
+++ src/frontend/Source.ts
@@ -1,9 +1,95 @@
+import { Readable } from "stream";
+
 // TODO:
 // # Implement Source using streams that will implement the interface Source on page 18.
 //   ## The readable stream with the source should be in paused (or pull) mode. Make sure that you don't add `data` event handler to keep the readable stream in paused mode.
 //   ## Verify that the readable stream `readableObjectMode` is `false`. The Source class is not able to handle object mode readable streams.
 
-// TODO: First test:
-//   - With input "A"
-//     - Source should return "A" when currentChar is called.
-//     - When nextChar is called Source should return EOL.
+// TODO: Should currentChar return EOF after nextChar has returned EOF?
+// TODO: Wrap source into async createSource function to allow for reading in the first chunk.
+
+enum InputState {
+  Initializing,
+  Readable,
+  Consumed
+}
+
+export class Source {
+  private input: Readable;
+  private state: InputState = InputState.Initializing;
+  private buffer: Buffer | undefined;
+  private index: number = -1;
+  private readableCb: () => void;
+
+  constructor(i: Readable) {
+    this.input = i;
+    // tslint:disable-next-line:no-empty
+    this.readableCb = () => {};
+    this.input.on("readable", () => {
+      console.log("readable callback");
+      this.state = InputState.Readable;
+      this.readableCb();
+    });
+    this.input.on("end", () => {
+      console.log("end callback");
+      this.state = InputState.Consumed;
+    });
+  }
+
+  async currentChar(): Promise<number | null> {
+    if (this.index < 0) {
+      return this.nextChar();
+    } else {
+      if (!this.buffer) {
+        throw new Error("Internal error - Buffer not read.");
+      }
+      return this.buffer[this.index];
+    }
+  }
+
+  async nextChar(): Promise<number | null> {
+    const bufferConsumed: boolean =
+      !!this.buffer && this.index + 1 >= this.buffer.length;
+    console.log("nextChar");
+
+    if (this.state === InputState.Consumed && bufferConsumed) {
+      return null;
+    }
+    if (!this.buffer) {
+      this.buffer = await this.commenceReading();
+    }
+    if (bufferConsumed) {
+      this.buffer = await this.readNextChunk();
+      this.index++;
+      return this.currentChar();
+    } else {
+      this.index++;
+      return this.currentChar();
+    }
+  }
+
+  async commenceReading(): Promise<Buffer> {
+    console.log("commenceReading()");
+    if (this.state === InputState.Readable) {
+      return this.input.read();
+    } else {
+      return new Promise((resolve, reject) => {
+        this.whenReadable(() => {
+          resolve(this.input.read());
+        });
+      });
+    }
+  }
+
+  whenReadable(cb: () => void) {
+    this.readableCb = cb;
+  }
+
+  async readNextChunk(): Promise<Buffer> {
+    console.log("readNextChunk()");
+    this.index = -1;
+    const chunk = this.input.read();
+    this.buffer = chunk;
+    return chunk;
+  }
+}
---
+++ test/frontend/SourceTest.ts
@@ -0,0 +1,69 @@
+import test from "ava";
+import { createReadStream } from "fs";
+import { Readable } from "stream";
+import { Source } from "../../src/frontend/Source";
+
+test("File can be read using stream", async t => {
+  const chunks: any[] = [];
+  const inputStream = createReadStream("ts_build/src/cli/ArgumentList.js");
+
+  const readChunk = () => {
+    const chunk = inputStream.read();
+    if (chunk) {
+      chunks.push(chunk);
+      setTimeout(readChunk, 1);
+    }
+  };
+
+  const p = new Promise<any[]>((resolve, reject) => {
+    inputStream.on("readable", () => {
+      setTimeout(readChunk, 1);
+    });
+
+    inputStream.on("end", () => {
+      resolve(chunks);
+    });
+  });
+
+  const data = await p;
+  t.is(data.length, 1);
+  const c = data[0];
+  console.log(typeof c);
+  console.log(c instanceof Buffer);
+  for (let i = 0; i < 20; ++i) {
+    if (c[i] === 0x000a) {
+      console.log("EOL!");
+    }
+    console.log(c[i]);
+  }
+});
+
+// TODO: First test:
+//   - With input "A"
+//     - Source should return "A" when currentChar is called.
+//     - When nextChar is called Source should return EOF.
+//   - With input "A"
+//     - Source should return "A" when nextChar is called.
+//     - When nextChar is called Source should return EOF.
+//   - With input "A"
+//     - Source should return "A" when nextChar is called.
+//     - Source should return "A" when currentChar is called.
+//     - When nextChar is called Source should return EOF.
+//   - With input "A"
+//     - Source should return "A" when currentChar is called.
+//     - Source should return "A" when currentChar is called.
+//     - When nextChar is called Source should return EOF.
+// TODO: Test that nextChar works correctly over buffer boundaries.
+test("currentChar followed by nextChar consumes source", async t => {
+  const inStream = new Readable({
+    // tslint:disable-next-line:no-empty
+    read() {}
+  });
+  inStream.push("A");
+  inStream.push(null);
+  const source = new Source(inStream);
+  t.is(await source.currentChar(), "A".charCodeAt(0));
+  t.is(await source.nextChar(), null);
+});
+
+//"use strict";␊
