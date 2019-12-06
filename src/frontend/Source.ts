8a1e79e9850786c39732fb34fde423f483c3fd99
Tuomas Järvensivu <tuomas.jarvensivu@iki.fi>
2019-12-06 22:14:54 +0200

    Remove comments

--- src/frontend/Source.ts
+++ src/frontend/Source.ts
@@ -1,10 +1,5 @@
 import { Readable } from "stream";
 
-// TODO:
-// # Implement Source using streams that will implement the interface Source on page 18.
-//   ## The readable stream with the source should be in paused (or pull) mode. Make sure that you don't add `data` event handler to keep the readable stream in paused mode.
-//   ## Verify that the readable stream `readableObjectMode` is `false`. The Source class is not able to handle object mode readable streams.
-
 enum InputState {
   Initializing,
   Readable,
